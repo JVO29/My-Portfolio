5bec8e3a4ef3cd0774e3ca0dd2e35a887f801c8f
JVO29 <zansanni@gmail.com>
2024-05-17 16:08:33 -0700

    Fourth Commit

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,7 +1,8 @@
 import logo from "../assets/SH Logo.png"
 import { FaLinkedin } from "react-icons/fa"
 import { FaGithub } from "react-icons/fa"
-
+import { BrowserRouter, Link } from "react-router-dom"
+import IconButton from "./IconButtons.jsx";
 
 const NavBar = () => {
     return (
@@ -9,10 +10,21 @@ const NavBar = () => {
             <div className="flex flex-shrink-0 items-center">
                 <img src={logo} alt={"Logo"}/>
             </div>
-            <div className="flex gap-4 text-5xl">
-                <FaLinkedin/>
-                <FaGithub/>
-            </div>
+            <BrowserRouter>
+                <div className="flex gap-4">
+                    <Link to={"https://www.linkedin.com/in/hassansanni/"}>
+                        <IconButton text={"LinkedIn"}>
+                            <FaLinkedin size={40}/>
+                        </IconButton>
+                    </Link>
+                    <Link to={"https://github.com/JVO29"}>
+                        <IconButton text={"Github"}>
+                            <FaGithub size={40}/>
+                        </IconButton>
+                    </Link>
+                </div>
+            </BrowserRouter>
+
         </nav>
     )
 }
